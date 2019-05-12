5656185a38e65441d3083305df0682ff7ef741e7
aleoreh <aleoreh@gmail.com>
2019-05-12 19:14:18 +0300

    Adjust and fix building

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task("css", function () {
     .pipe(less())
     .pipe(postcss([autoprefixer()]))
     .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
@@ -35,13 +36,13 @@ gulp.task("images", function () {
       imagemin.jpegtran({ progressive: true }),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("sprite", function () {
@@ -96,7 +97,7 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series(
+gulp.task("dev", gulp.series(
   "clean",
   "copy",
   "css",
@@ -104,4 +105,10 @@ gulp.task("build", gulp.series(
   "html"
 ));
 
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("build", gulp.series(
+  "dev",
+  "images",
+  "webp"
+));
+
+gulp.task("start", gulp.series("dev", "server"));
